eff26d06582dbc08fba5cba4c1c195c78d7b0f8f
peaboard <peaboard.mail@gmail.com>
2018-07-09 23:40:22 +0530

    Fixed hover

--- js/main.js
+++ js/main.js
@@ -8,23 +8,23 @@ var timer = 20000;
 
 //when the document has loaded
 $(document).ready(function(){
-	
+
 	//some preconfig, getting the browser container details also setting the fadingout the options
 	//also setting the timer.
-	$(".topOver").fadeOut();	
-	document.getElementById('timer').value = timer/1000;	
+	//$(".topOver").fadeOut();
+	document.getElementById('timer').value = timer/1000;
 	pwidth = $(window).width();
 	pheight = $(window).height();
-	$('#allContent').mouseleave(showDetailsEnd);
+	//$('#allContent').mouseleave(showDetailsEnd);
 
-	
-	
+
+
 	//the timer
 	changeImage();
-	
+
 	//when the window is resized, it will update the width and height
 	$(window).resize(function(){
-		pwidth = $(window).width(); 
+		pwidth = $(window).width();
 		pheight = $(window).height();
 	});
 });
@@ -35,38 +35,41 @@ function changeImage()
 {
 
 	$("#mainImage").fadeOut();
-	
+
 	//check if the blur checkbox is ticked
 	if (document.getElementById('blur').checked) {
         $('#mainImage').attr('src', 'https://unsplash.it/' + pwidth + '/' + pheight + '/?random&blur&' + backgroundCount);
     } else {
 		$('#mainImage').attr('src', 'https://unsplash.it/' + pwidth + '/' + pheight + '/?random&' + backgroundCount);
 	}
-	
+
 	//when image loaded, start the timer and show image
 	$('#mainImage').on('load', function(){
 		$("#mainImage").fadeIn();
 		restartTimer();
 	});
 	backgroundCount++;
+	//showDetails();
 }
 
 //when hovering over an image show the details.
 function showDetails()
 {
-	$(".topOver").fadeIn();
+	//$(".topOver").fadeIn();
+	document.getElementById('settings').style.opacity=1;
 }
 
 
 //when not hovering over an image hide details.
 function showDetailsEnd()
-{	
-	$(".topOver").fadeOut();
+{
+	//$(".topOver").fadeOut();
+	document.getElementById('settings').style.opacity=0;
 }
 
 //when the timer is changed, set the new timer variable to what the user has entered, no input validation though
 function setTimer()
-{	
+{
 	var value = document.getElementById('timer').value;
 	timer = value * 1000;
 }
@@ -75,6 +78,6 @@ function setTimer()
 function restartTimer()
 {
 	timePrompt = setTimeout(function(){
-		changeImage();	
+		changeImage();
 	}, timer);
 }
